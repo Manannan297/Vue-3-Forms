4daf63878515b57857849a904a1429f57ae8824a
Evgenia Isaeva <evgenia.isaeva297@gmail.com>
2021-10-18 22:56:29 +0300

    #9 Basic a11y for our components

--- src/components/BaseRadio.vue
+++ src/components/BaseRadio.vue
@@ -1,15 +1,18 @@
 <template>
   <input
     type="radio"
+    :id="uuid"
     :checked="modelValue === value"
     :value="value"
     v-bind="$attrs"
     @change="$emit('update:modelValue', value)"
   />
-  <label v-if="label">{{ label }}</label>
+  <label :for="uuid" v-if="label">{{ label }}</label>
 </template>
 
 <script>
+import UniqueID from '../features/UniqueID';
+
 export default {
   props: {
     label: {
@@ -23,6 +26,12 @@ export default {
     value: {
       type: [String, Number],
       required: true
+    }
+  },
+  setup () {
+    const uuid = UniqueID().getID()
+    return {
+      uuid
     }
   }
 }
